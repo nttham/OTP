d8a562ebc7bf9b8075aaec4fc755b42adf6a73a3
nttham <Neethu.TThampi@cognizant.com>
2016-05-26 18:10:17 +0530

    updated the comments
    createDecipher

--- otp.js
+++ otp.js
@@ -2,7 +2,7 @@
  * Created by 423919 on 5/18/2016.
  * This is the Otp module which generate a token and validate the same
  */
-
+// dependencies for this module
 var crypto = require('crypto');
 var cipherPwd = 'oyeilyodd';
 var encryptionType = 'aes192';
@@ -10,6 +10,7 @@ var randomString = require("randomstring");
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 var config = require('./config.json');
+//constructor
 var Otp = function () {
 
 };
@@ -19,29 +20,35 @@ var Otp = function () {
 // time
 
 Otp.prototype.generateOtp = function (app) {
-
+    // route for generate Otp
     app.post("/generate", jsonParser, function (req, res) {
 
         var otpOptions = {};
+        //creating the otpOptions from the config.json
         otpOptions.length = (config.otp && config.otp.otpLength) ? config.otp.otpLength : 4;
         otpOptions.charset = (config.otp && config.otp.otpType) ? config.otp.otpType : 'numeric';
         var expiryTime = (config.otp && config.otp.otpExpiryTime) ? config.otp.otpExpiryTime : 10;
         var otpCode = randomString.generate(otpOptions);
-        // generate a secretKey for Otp with otp and OTP generated Time
+       
+        //otpGenTime will have current time in milliseconds
         var otpGenTime = Date.now();
+        //generating the expiry time in milliseconds
         var otpExpiryTime = otpGenTime + (expiryTime * 60000);
+        //generating the secret data for the key which is a combination
+        // of otpcode and expiry time in the format ("otpCode-otpExpiryTime")
         var secretData = otpCode + '-' + otpExpiryTime;
 
-        // encrypting the secretData
+        // encrypting the secretData with the cipherPwd
         var cipher = crypto.createCipher(encryptionType, cipherPwd);
         var encrypted = cipher.update(secretData, 'utf8', 'hex');
         encrypted += cipher.final('hex');
 
-
+        // checking the channel in config.json 
         if (config.channel === 'twilio') {
-
+            //hooking the twilio to OTP
             var twilio = require("./twilioservice.js");
             var twilioObj = new twilio();
+            //creating the options for twilio
             var msgObj = {
                 "accountSID": config.twilio.accountSID,
                 "authToken": config.twilio.authToken,
@@ -50,6 +57,7 @@ Otp.prototype.generateOtp = function (app) {
                 "body": "OTP pin is " + otpCode
 
             };
+            
             twilioObj.sendMessage(msgObj, function (err, result) {
                 res.header("Access-Control-Allow-Origin", "*");
                 res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -72,7 +80,9 @@ Otp.prototype.generateOtp = function (app) {
 
             });
         } else if (config.channel === 'sendgrid') {
+            //hooking the sendgrid to OTP
             var sendmailObj = new require("./sendgridservice.js")();
+            //creating the options for sendgrid
             var msgObj = {
                 "accountSID": config.sendgrid.accountSID,
                 "authToken": config.sendgrid.authToken,
@@ -105,8 +115,9 @@ Otp.prototype.validateOtp = function (app) {
 
     app.post("/validate", jsonParser, function (req, res) {
 
-
+        //creating the Decipher with the cipherPwd
         var decipher = crypto.createDecipher(encryptionType, cipherPwd);
+        // gets the otp key 
         var encrypted = req.body.otpKey;
         // decrypting the key to get the OTP and expiry time which comes in "OTP-EXPIRYTIME" format
         var decrypted = decipher.update(encrypted, 'hex', 'utf8');
